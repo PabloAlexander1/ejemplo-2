bc2cd86de1e72e9b0f20aa579a1a8e5a3338bc5e
Pablo  Arteaga Fuentes <166053425+PabloAlexander1@users.noreply.github.com>
2024-07-22 15:02:41 +0000

    imprimir fecha

---
+++ imprimir_fecha.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+public class imprimir_fecha {
+     public static void main(String[] args) {
+        // Obtener la fecha de hoy
+        LocalDate fechaDeHoy = LocalDate.now();
+        
+        // Imprimir la fecha de hoy
+        System.out.println("La fecha de hoy es: " + fechaDeHoy);
+}
+    }
